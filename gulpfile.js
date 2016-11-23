5d55d281e4d3f1737c2e763720d564be82991eda
Philip Kelly <pkelly@xogrp.com>
2016-11-23 11:41:19 -0500

    Add Jekyll and SASS placeholder import test
    - Add node-sass-import-once to prevent dup css due to the same sass
    file being imported.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,35 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var importOnce = require('node-sass-import-once');
 
 var config = {
     bootstrapDir: './node_modules/bootstrap',
-    publicDir: './public',
+    publicDir: './stylesheets',
+    srcFile: './css/app.scss'
 };
 
 gulp.task('css', function() {
-    return gulp.src('./css/app.scss')
+    return gulp.src(config.srcFile)
     .pipe(sass({
-        includePaths: [config.bootstrapDir + '/scss'],
-    }))
-    .pipe(gulp.dest(config.publicDir + '/css'));
+      includePaths: [config.bootstrapDir + '/scss'],
+      outputStyle: 'nested',
+      importer: importOnce,
+      importOnce: {
+        index: false,
+        css: false,
+        bower: false
+      }
+    }).on('error', sass.logError))
+    .pipe(gulp.dest(config.publicDir));
 });
 
 gulp.task('fonts', function() {
     return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
     .pipe(gulp.dest(config.publicDir + '/fonts'));
+});
+
+gulp.task('default', function() {
+  gulp.watch('./css/**/*.scss', ['css']);
 });
